7ab8119d03af3c658b1d7293e8c1555b307d7137
Arash Altafi <arashaltafi1377@gmail.com>
2024-11-09 14:11:56 +0330

    add basicMarquee

--- app/src/main/java/com/arash/altafi/englishteachercompose/ui/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/arash/altafi/englishteachercompose/ui/presentation/splash/SplashScreen.kt
@@ -1,6 +1,8 @@
 package com.arash.altafi.englishteachercompose.ui.presentation.splash
 
+import androidx.compose.foundation.MarqueeAnimationMode
 import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
@@ -10,6 +12,8 @@ import androidx.compose.ui.unit.*
 import kotlinx.coroutines.delay
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.ui.Alignment
 import androidx.navigation.NavController
 import com.arash.altafi.englishteachercompose.utils.Constant
@@ -17,7 +21,7 @@ import com.arash.altafi.englishteachercompose.utils.Constant
 @Composable
 fun SplashScreen(navController: NavController) {
     LaunchedEffect(Unit) {
-        delay(5000)
+        delay(500000)
         navController.navigate(Constant.Routes.LEARN) {
             popUpTo(Constant.Routes.SPLASH) {
                 inclusive = true
@@ -31,6 +35,9 @@ fun SplashScreen(navController: NavController) {
         contentAlignment = Alignment.Center
     ) {
         Text(
+            modifier = Modifier
+                .width(200.dp)
+                .basicMarquee(animationMode = MarqueeAnimationMode.Immediately),
             text = "Splash Screen",
             color = Color.White,
             fontSize = 32.sp,
